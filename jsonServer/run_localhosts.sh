09564a48409de46fc8773d4a2376da9cc6c0a855
ntiman <ntiman1996@gmail.com>
2024-05-10 00:47:23 -0400

    run the second jsonserver, separate routes

--- jsonServer/run_localhosts.sh
+++ jsonServer/run_localhosts.sh
@@ -1,4 +1,4 @@
 source ~/.bash_profile
 
-json-server --watch cameraAPI.json --port 7071 --nc false --routes routes.json
-json-server --watch orgAPI.json --port 7074 --nc false --routes routes.json
+json-server --watch cameraAPI.json --port 7071 --nc false --routes routesCamera.json
+json-server --watch orgAPI.json --port 7074 --nc false --routes routesOrg.json
