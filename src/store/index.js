0fcec8db7bd467887b60af4133b0f598707023fd
ntiman <ntiman1996@gmail.com>
2024-05-09 23:06:28 -0400

    refactor: rename slices to match routes

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import scansReducer from "./slices/scansSlice";
+import scansReducer from "./slices/eventsSlice";
 
 export const store = configureStore({
     reducer: {
