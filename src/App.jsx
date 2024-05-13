1040ecbf6f5b4afa8fc594b50b267669bf20672e
ntiman <ntiman1996@gmail.com>
2024-05-12 20:22:41 -0400

    refactor: wrap router around the wrapper <section>

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,18 @@ import NavigationBar from "./components/NavigationBar";
 
 function App() {
   return (
-    <section className="flex flex-col mx-auto bg-background text-white overflow-y-scroll min-h-screen">
-      <Router>
-      <NavigationBar />
-      <section className="px-4  pt-8">
-        <Routes>
-          <Route index path="/" element={<Home />} />
-          <Route path="/accounts" element={<Accounts />} />
-          <Route path="/settings" element={<Settings />} />
-        </Routes>
+    <Router>
+      <section className="flex flex-col mx-auto bg-background text-white overflow-y-scroll min-h-screen">
+        <NavigationBar />
+        <section className="px-4 pt-8 bg-card">
+          <Routes>
+            <Route index path="/" element={<Home />} />
+            <Route path="/accounts" element={<Accounts />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
         </section>
-      </Router>
-    </section>
+      </section>
+    </Router>
   );
 }
 
